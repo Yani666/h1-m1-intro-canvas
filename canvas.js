15f47d1558ac063208fdb0a42324ede684ae1950
Yani <yanis051989@gmail.com>
2022-03-29 16:49:53 -0500

    terminamos canvas

--- canvas.js
+++ canvas.js
@@ -1,5 +1,26 @@
-console.log("holi")
+// index.js
 
-console.log("isisis alguien ahi")
+const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d")
 
-console.log("estoy en otra rama")
+//rectangulo
+ctx.fillStyle =  "purple"
+
+//fillRect(x, y, width, height)
+ctx.fillRect(260, 260, 30, 30)
+
+ctx.fillRect(25,25,100,100)
+ctx.clearRect(45, 45, 60, 50)
+ctx.strokeRect(300, 50, 50, 50)
+
+ctx.fillStyle = "red"
+ctx.fillRect(25,25,100,100)
+//paths
+ctx.beginPath()
+//inicio de posicion
+ctx.moveTo(450,150)
+//fin de posicion
+ctx.lineTo(450,50)
+//ejecucion de pintado
+ctx.stroke()
+ctx.closePath()
